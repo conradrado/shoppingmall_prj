24907568c1fe47a9b782434c1e9e0ddfc2661efc
conradrado <gimmy6707@gmail.com>
2022-01-10 22:13:17 +0900

    앱 생성

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+class Post(models.Model)
+    title = models.Charfield(max_length = 30)
+    content = models.TextField()
 
+    created_at = models.DateTimeField()
 # Create your models here.
